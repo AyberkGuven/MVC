3f3029969e199c8346e6664d5bc69429cb11f262
Ayberk Güven <90345997+AyberkGuven@users.noreply.github.com>
2022-02-16 16:47:57 +0300

    FirstMVC

---
+++ FirstMVC/FirstMVC/Context/DbMesaj.cs
@@ -0,0 +1,19 @@
+﻿using FirstMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FirstMVC.Context
+{
+    public class DbMesaj:DbContext
+    {
+        public DbMesaj()
+            : base("AistekDBConnectionString")
+        {
+
+        }
+        public DbSet<Mesaj> Mesajs { get; set; }
+    }
+}
