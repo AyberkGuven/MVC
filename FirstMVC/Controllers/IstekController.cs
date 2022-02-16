475b6e32518546ff538c80fbf6aea27903095433
Ayberk Güven <90345997+AyberkGuven@users.noreply.github.com>
2022-02-16 16:43:47 +0300

    FirstMVC

---
+++ FirstMVC/Controllers/IstekController.cs
@@ -0,0 +1,29 @@
+﻿using FirstMVC.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVC.Controllers
+{
+    public class IstekController : Controller
+    {
+        //
+        // GET: /Istek/
+        public ActionResult Index()
+        {
+            DbMesaj db = new DbMesaj();
+            var request = db.Mesajs.ToList();
+            return View(request);
+        }
+        public ActionResult Update()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+	}
+}
