08c547b711ab4ed6166e3cd9872dd79d66038d57
Ayberk Güven <90345997+AyberkGuven@users.noreply.github.com>
2022-02-17 15:09:55 +0300

    Add files via upload

--- FirstMVC/FirstMVC/Controllers/IstekController.cs
+++ FirstMVC/FirstMVC/Controllers/IstekController.cs
@@ -1,4 +1,5 @@
 ﻿using FirstMVC.Context;
+using FirstMVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,13 +18,50 @@ namespace FirstMVC.Controllers
             var request = db.Mesajs.ToList();
             return View(request);
         }
-        public ActionResult Update()
-        {
-            return View();
-        }
+        //public ActionResult Update()
+        //{
+        //    return View();
+        //}
+        [HttpGet]
         public ActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Mesaj mesaj)
+        {
+            DbMesaj db = new DbMesaj();
+            db.Mesajs.Add(mesaj);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            DbMesaj db = new DbMesaj();
+            var mesaj = db.Mesajs.Find(id);
+            return View(mesaj);
+        }
+        [HttpPost]
+        public ActionResult Edit(Mesaj mesaj)
+        {
+            DbMesaj db = new DbMesaj();
+            var data = db.Mesajs.Find(mesaj.Id);
+            data.Name = mesaj.Name;
+            data.Surname = mesaj.Surname;
+            data.Phone = mesaj.Phone;
+            data.Mail = mesaj.Mail;
+            data.Request = mesaj.Request;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            DbMesaj db = new DbMesaj();
+            var Del = db.Mesajs.Find(id);
+            db.Mesajs.Remove(Del);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 	}
 }
