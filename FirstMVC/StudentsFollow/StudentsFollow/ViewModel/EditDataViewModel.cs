965bbe9f34137b67c94813ea452779edd1536450
Ayberk Güven <90345997+AyberkGuven@users.noreply.github.com>
2022-03-09 15:55:18 +0300

    Add files via upload

---
+++ FirstMVC/StudentsFollow/StudentsFollow/ViewModel/EditDataViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentsFollow.ViewModel
+{
+    public class EditDataViewModel
+    {
+        public EditStudentViewModel EditStudentViewModel { get; set; }
+        public List<ClassRoomViewModel> ClassRoomViewModel { get; set; }
+    }
+}
---
+++ FirstMVC/StudentsFollow/StudentsFollow/ViewModel/EditStudentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentsFollow.ViewModel
+{
+    public class EditStudentViewModel
+    {
+        public string ImagePatch { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public bool Gender { get; set; }
+        public byte ClassRoomId { get; set; }
+    }
+}
